672e3c7cec511cbca2d0a1165e26fef6e48ffd9b
Lawrence Lucky <43374545+lawrencelucky@users.noreply.github.com>
2021-11-15 21:29:21 +0100

    Express (#1)
    * Adding basic routing and parsing incoming requests using body-parser
    
    * Splitting the app to have it's own routes folder
    
    * Adding a 404 page not found
    
    * filtering routes path
    
    * adding and serving html pages
    
    * adding a util function to navigate file paths
    
    * Adding styles to our pages and serving them statitically.
    
    Co-authored-by: lawrencelucky <lawrencelucky@gmail.com>

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-const http = require('http');
+const path = require('path');
 
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-const server = http.createServer(app);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-server.listen(3000);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000);
