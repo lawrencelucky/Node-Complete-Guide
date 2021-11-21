5b56675ce963eaa3967791d3c0fec2af7ff4f06c
lawrencelucky <lawrencelucky@gmail.com>
2021-11-21 15:21:18 +0100

    adding layouts in pug

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.render('404', { docTitle: 'Page Not Found' });
+  res.render('404', { pageTitle: 'Page Not Found' });
 });
 
 app.listen(3000);
