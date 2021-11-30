976e6556808ef4b50943e7e230179e94cfd28968
Lawrence Lucky <43374545+lawrencelucky@users.noreply.github.com>
2021-11-30 11:22:42 +0100

    mvc (#3)
    * Adding basic routing and parsing incoming requests using body-parser
    
    * Splitting the app to have it's own routes folder
    
    * Adding a 404 page not found
    
    * filtering routes path
    
    * adding and serving html pages
    
    * adding a util function to navigate file paths
    
    * Adding styles to our pages and serving them statitically.
    
    * using the pug template engine to display dynamic content
    
    * adding layouts in pug
    
    * adding the handlebars template engine
    
    * adding the ejs template engine
    
    * adding the controllers
    
    * adding the model and saving data to file
    
    Co-authored-by: lawrencelucky <lawrencelucky@gmail.com>

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const adminData = require('./routes/admin');
-
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+const errorController = require('./controllers/error');
+
 
 const app = express();
 
@@ -15,12 +17,10 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.render('404', { pageTitle: 'Page Not Found' });
+app.use(errorController.get404);
 
-});
 
 app.listen(3000);
